1b24e7f94be30d7122a95bd59e6c70b3a228c79d
bleatingchef <ananddurgesh701@gmail.com>
2024-05-28 14:43:12 +0530

    contact_page

--- src/pages/Contact_com.jsx
+++ src/pages/Contact_com.jsx
@@ -1,23 +1,23 @@
 // src/pages/Home.jsx
 
 import React from 'react';
-import Hero from "../components/Hero";
 import Benefits from "../components/Benefits";
 import Collaboration from "../components/Collaboration";
-import Services from "../components/Services";
 import Pricing from "../components/Pricing";
 import Roadmap from "../components/Roadmap";
 import Footer from "../components/Footer";
+import Contact_hero from '../components/Contact_hero';
+import Contact_services from '../components/Contact_services';
 
 const Contact_com = () => {
   return (
     <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-      <Hero />
-      <Benefits />
-      <Collaboration />
-      <Services />
-      <Pricing />
-      <Roadmap />
+      <Contact_hero/>
+      {/* <Benefits /> */}
+      {/* <Collaboration /> */}
+      <Contact_services/>
+      {/* <Pricing /> */}
+      {/* <Roadmap /> */}
       <Footer />
     </div>
   );
