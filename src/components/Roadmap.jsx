0a3afddd777530081212ad0de6af6385da622488
Himanshu behera <himanshubehera08052003@gmil.com>
2024-06-01 15:48:11 +0530

    gradiant changes

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -17,9 +17,8 @@ const Roadmap = () => (
 
           return (
             <div
-              className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] ${
-                item.colorful ? "bg-conic-gradient" : "bg-n-6"
-              }`}
+              className={`md:flex even:md:translate-y-[7rem] p-0.25 rounded-[2.5rem] bg-conic-gradient
+              `}
               key={item.id}
             >
               <div className="relative p-8 bg-n-8 rounded-[2.4375rem] overflow-hidden xl:p-15">
